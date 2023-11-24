aaf5397ad75eb7e601db9acd3ee08dae2d7ffb31
Ivan Moreno <ivanmoreno1998@gmail.com>
2023-11-24 09:16:12 +0100

    Logic  implemented, Dockerfile creation

--- Makefile
+++ Makefile
@@ -49,19 +49,19 @@ help:
 	@echo
 
 init:
-	@python3 -m venv .venv && source .venv/bin/activate && python3 -m pip install -r requirements.txt && deactivate
+	@python3 -m venv .venv && . .venv/bin/activate && python3 -m pip install -r requirements.txt && deactivate
 
 check:
-	@source .venv/bin/activate && python3 -m flake8 app/ ${TARGET_TEST} ${TARGET_FUNCTIONAL_TEST} && deactivate
+	@. .venv/bin/activate && python3 -m flake8 app/ ${TARGET_TEST} ${TARGET_FUNCTIONAL_TEST} && deactivate
 
 build:
 	@docker build . -t ${PROJECT_NAME}-${ENV} 
 
 test:
-	@source .venv/bin/activate && pytest ${COV} ${TARGET_TEST} && deactivate
+	@. .venv/bin/activate && pytest ${COV} ${TARGET_TEST} && deactivate
 
 functional_test:
-	@source .venv/bin/activate && pytest ${TARGET_FUNCTIONAL_TEST} && deactivate
+	@. .venv/bin/activate && pytest ${TARGET_FUNCTIONAL_TEST} && deactivate
 
 run:
-	@source .venv/bin/activate && uvicorn app.main:app --reload && deactivate
+	@. .venv/bin/activate && uvicorn app.main:app --reload --log-level debug && deactivate
--- app/endpoints/v0_endpoints.py
+++ app/endpoints/v0_endpoints.py
@@ -2,33 +2,30 @@ from fastapi import HTTPException, APIRouter
 from app.classes.usuario import Usuario
 from app.db_connection import MongoDBConnection
 from app import DB_USER, URI_PASSWORD, DB_NAME, COLLECTION_NAME
+import logging
 
-
+logger = logging.getLogger(__name__)
 router = APIRouter()
 
 mongo = MongoDBConnection(DB_USER, URI_PASSWORD, DB_NAME, COLLECTION_NAME)
 
-usuarios_db = {}
-
 
 @router.post("/v0/fichar")
 async def fichar(usuario: Usuario):
+    logger.info(f"Usuario: {usuario.user}")
     # Verifica si el usuario ya existe en la base de datos
-
-    if usuario.user in usuarios_db:
+    if mongo.find_user({"user": usuario.user}):
         raise HTTPException(status_code=400, detail="El usuario ya existe")
-
-    # Almacena el usuario en la base de datos (en este caso, en memoria)
-    usuarios_db[usuario.user] = usuario
-
-    return {"mensaje": f"{usuario.user} ha fichado correctamente"}
+    else:
+        mongo.insert_user(usuario.dict())
+        return {"mensaje": f"{usuario.user} ha fichado correctamente"}
 
 
 @router.post("/v0/desfichar")
 async def desfichar(usuario: Usuario):
+    logger.debug(f"Usuario: {usuario.user}")
     # Verifica si el usuario ya existe en la base de datos
-    if usuario.user in usuarios_db:
-        user = usuarios_db.pop(usuario.user)
-        return {"mensaje": f"{user.user} ha desfichado correctamente"}
-
-    raise HTTPException(status_code=400, detail="El usuario no existe")
+    response = mongo.delete_user({"user": usuario.user})
+    if response.deleted_count == 0:
+        raise HTTPException(status_code=404, detail="El usuario no existe")
+    return {"mensaje": f"{usuario.user} ha desfichado correctamente"}
